ff703f8ac8cccd413607fa2b35fdebaae44127f7
Sören Michaels <soeren.michaels@gmail.com>
2022-06-04 18:28:58 +0200

    husky

--- Backend/.eslintrc.cjs
+++ Backend/.eslintrc.cjs
@@ -2,5 +2,9 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
 };
