ac02a46a5db2f042c03c899bf86cf477cf0af70f
Sören Michaels <soeren.michaels@gmail.com>
2022-06-05 20:21:30 +0200

    formatting

--- Frontend/src/lib/js/socket.ts
+++ Frontend/src/lib/js/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 
 const socket: Socket = io(import.meta.env.VITE_DOMAIN, {
-  closeOnBeforeunload: false // defaults to true,
+	closeOnBeforeunload: false // defaults to true,
 });
 
 export default socket;
--- Frontend/src/lib/js/supabaseClient.js
+++ Frontend/src/lib/js/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const SUPABASE_KEY =import.meta.env.VITE_SUPABASE_KEY
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
