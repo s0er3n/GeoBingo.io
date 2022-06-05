ff4f73f98a29cb5a67a798c13f4d4bd8a761a2ae
Sören Michaels <soeren.michaels@gmail.com>
2022-06-05 20:21:13 +0200

    moved supabase url and key in env

--- Frontend/src/lib/js/supabaseClient.js
+++ Frontend/src/lib/js/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const SUPABASE_KEY =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDk4Nzc4NCwiZXhwIjoxOTUwNTYzNzg0fQ.pvNjymNGXdj4HTKsHUsN3uhZNJr9FYKbXpS7Xyxhq2A';
-const SUPABASE_URL = 'https://aesfbivwcjawvnujxazv.supabase.co';
+const SUPABASE_KEY =import.meta.env.VITE_SUPABASE_KEY
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
