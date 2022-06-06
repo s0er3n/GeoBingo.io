ee98eb38ed409a81e40aa81fdb82cf05263521bb
Sören Michaels <soeren.michaels@gmail.com>
2022-06-06 14:26:01 +0200

    formatting

--- Frontend/src/lib/js/supabaseClient.js
+++ Frontend/src/lib/js/supabaseClient.js
@@ -2,6 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 if (!(SUPABASE_URL && SUPABASE_KEY)) {
-  console.warn("no supabase key or/and url")
+	console.warn('no supabase key or/and url');
 }
-export const supabase = SUPABASE_URL && SUPABASE_KEY ? createClient(SUPABASE_URL, SUPABASE_KEY) : undefined
+export const supabase =
+	SUPABASE_URL && SUPABASE_KEY
+		? createClient(SUPABASE_URL, SUPABASE_KEY)
+		: undefined;
