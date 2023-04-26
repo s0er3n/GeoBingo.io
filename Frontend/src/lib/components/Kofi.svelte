0c06b8743f63f59063fdecf7a0368d195813ee03
Sören Michaels <soeren.michaels@gmail.com>
2023-04-26 21:49:09 +0200

    less socials

--- Frontend/src/lib/components/Kofi.svelte
+++ Frontend/src/lib/components/Kofi.svelte
@@ -3,7 +3,7 @@
   let kofi;
   if (browser) {
     try {
-      kofiwidget2.init("Buy me a coffee <3", "#29abe0", "F1F068LGZ");
+      kofiwidget2.init("donate", "#29abe0", "F1F068LGZ");
       kofi = kofiwidget2.getHTML() ?? "";
     } catch (e) {
       console.log(e);
--- Frontend/src/lib/components/Socials.svelte
+++ Frontend/src/lib/components/Socials.svelte
@@ -33,29 +33,29 @@
         <TwitchIcon size="1x" /> Twitch
       </div>
     </a>
-    <a href="https://www.tiktok.com/@geobingo" target="_blank">
-      <img
-        src="TikTok.svg"
-        class="p-2 rounded-md h-10 shadow-md bg-slate-100"
-      />
-    </a>
-    <a
-      href="https://www.youtube.com/channel/UCEB92TvBTby2GeDyU2ak8Dg"
-      target="_blank"
-    >
-      <div
-        class="p-2 rounded-md h-10 shadow-md bg-red-700  flex justify-center items-center gap-1"
-      >
-        <YoutubeIcon size="1x" /> YouTube
-      </div>
-    </a>
-    <a href="https://www.instagram.com/geobingo.io/" target="_blank">
-      <div
-        class="p-2 rounded-md h-10 shadow-md bg-slate-500  flex justify-center items-center gap-1"
-      >
-        <InstagramIcon size="1x" /> Instagram
-      </div>
-    </a>
+    <!-- <a href="https://www.tiktok.com/@geobingo" target="_blank"> -->
+    <!--   <img -->
+    <!--     src="TikTok.svg" -->
+    <!--     class="p-2 rounded-md h-10 shadow-md bg-slate-100" -->
+    <!--   /> -->
+    <!-- </a> -->
+    <!-- <a -->
+    <!--   href="https://www.youtube.com/channel/UCEB92TvBTby2GeDyU2ak8Dg" -->
+    <!--   target="_blank" -->
+    <!-- > -->
+    <!--   <div -->
+    <!--     class="p-2 rounded-md h-10 shadow-md bg-red-700  flex justify-center items-center gap-1" -->
+    <!--   > -->
+    <!--     <YoutubeIcon size="1x" /> YouTube -->
+    <!--   </div> -->
+    <!-- </a> -->
+    <!-- <a href="https://www.instagram.com/geobingo.io/" target="_blank"> -->
+    <!--   <div -->
+    <!--     class="p-2 rounded-md h-10 shadow-md bg-slate-500  flex justify-center items-center gap-1" -->
+    <!--   > -->
+    <!--     <InstagramIcon size="1x" /> Instagram -->
+    <!--   </div> -->
+    <!-- </a> -->
     <Kofi />
   </div>
 </div>
